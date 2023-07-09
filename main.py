b49e9c1fe03a9b7bc9f54cec817ab729f436e02b
Beth Sirak <bethlehemsirak@outlook.com>
2023-07-09 19:41:33 +0100

    refactored code into one function caesar()

--- main.py
+++ main.py
@@ -8,33 +8,18 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-def encrypt(text,shift):
-  text_list = list(text.strip(""))
-  text_encoded = ""
-  for char in text_list:
-    if char in alphabet:
-      current_index = alphabet.index(char)
-      new_index = current_index + shift
-      new_char = alphabet[new_index]
-      text_encoded += new_char
-  print(f"The encoded text is {text_encoded}")
-encrypt(text,shift)
+def caesar(plain_text, shift_amount, cypher):
+  cipher_text = ""
+  if cypher.lower() == "decode":
+      shift_amount *= -1
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    cipher_text += alphabet[new_position]
+  print(f"The {direction}d text is {cipher_text}")
 
-def decrypt(text,shift):
-  text_list = list(text.strip(""))
-  text_encoded = ""
-  for char in text_list:
-      current_index = alphabet.index(char)
-      new_index = current_index - shift
-      if new_index < 0:
-                new_index = 26 - new_index
-      new_char = alphabet[new_index]
-      text_encoded += new_char
-  print(f"The decrypted text is {text_encoded}")
 
-if direction.lower() == "encode":
-  encrypt(text,shift)
-elif direction.lower() == "decode":
-  decrypt(text,shift)
-else:
-  print("You have entered an invalid option")
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+
+caesar(plain_text = text, shift_amount = shift, cypher = direction)
